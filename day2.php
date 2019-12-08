b2c701c3450171b55574ca807649bfa03514dd9e
markbennett1973 <mbennett@rawnet.com>
2019-12-08 09:46:24 +0000

    move computer to standalone file

--- day2.php
+++ day2.php
@@ -2,20 +2,19 @@
 
 declare(strict_types = 1);
 
+include('computer.php');
+
 const INPUT_FILE = 'day2input.txt';
-const OP_ADD = 1;
-const OP_MULTIPLY = 2;
-const OP_EXIT = 99;
 
 echo 'Part 1: ' . part1() . "\n";
 echo 'Part 2: ' . part2() . "\n";
 
 function part1(): string
 {
-    $registers = initRegisters();
+    $registers = loadProgram(INPUT_FILE);
     $registers[1] = 12;
     $registers[2] = 2;
-    runProgram($registers);
+    runProgram($registers, 0);
     return (string) $registers[0];
 }
 
@@ -26,10 +25,10 @@ function part2(): string
 
     for ($noun = 0; $noun <= $limit; $noun++) {
         for ($verb = 0; $verb <= $limit; $verb++) {
-            $registers = initRegisters();
+            $registers = loadProgram(INPUT_FILE);
             $registers[1] = $noun;
             $registers[2] = $verb;
-            runProgram($registers);
+            runProgram($registers, 0);
 
             if ($registers[0] === $target) {
                 return (string) (100 * $noun + $verb);
@@ -38,39 +37,4 @@ function part2(): string
     }
 
     return '';
-}
-
-function initRegisters(): array
-{
-    $input = file_get_contents(INPUT_FILE);
-    return explode(',', $input);
-}
-
-function runProgram(array &$registers)
-{
-    $stepToExecute = 0;
-
-    while ($registers[$stepToExecute] != OP_EXIT) {
-        $opCode = $registers[$stepToExecute];
-        $input1Pos = $registers[$stepToExecute + 1];
-        $input2Pos = $registers[$stepToExecute + 2];
-        $outputPos = $registers[$stepToExecute + 3];
-
-        switch ($opCode) {
-            case OP_ADD:
-                $registers[$outputPos] = $registers[$input1Pos] + $registers[$input2Pos];
-                break;
-
-            case OP_MULTIPLY:
-                $registers[$outputPos] = $registers[$input1Pos] * $registers[$input2Pos];
-                break;
-
-            default:
-                die('Unexpected op code at position ' . $stepToExecute . ': ' . $registers[$stepToExecute] . "\n");
-        }
-
-        $stepToExecute = $stepToExecute + 4;
-    }
-
-    return $registers;
 }
--- day5.php
+++ day5.php
@@ -2,19 +2,9 @@
 
 declare(strict_types = 1);
 
-const INPUT_FILE = 'day5input.txt';
-const OP_ADD = 1;
-const OP_MULTIPLY = 2;
-const OP_INPUT = 3;
-const OP_OUTPUT = 4;
-const OP_JUMP_IF_TRUE = 5;
-const OP_JUMP_IF_FALSE = 6;
-const OP_LESS_THAN = 7;
-const OP_EQUALS = 8;
-const OP_EXIT = 99;
+include('computer.php');
 
-const BY_REF = 0;
-const BY_VAL = 1;
+const INPUT_FILE = 'day5input.txt';
 
 // runTests();
 
@@ -23,18 +13,18 @@ echo 'Part 2: ' . part2() . "\n";
 
 function part1(): int
 {
-    $registers = initRegisters();
+    $program = loadProgram(INPUT_FILE);
     $input = 1;
-    return runProgram($registers, $input);
+    return runProgram($program, $input);
 }
 
 // part 1 5182797
 
 function part2(): int
 {
-    $registers = initRegisters();
+    $program = loadProgram(INPUT_FILE);
     $input = 5;
-    return runProgram($registers, $input);
+    return runProgram($program, $input);
 }
 
 function runTests()
@@ -66,141 +56,4 @@ function runTests()
     for ($input = 6; $input <= 10; $input++) {
         print "$input gives " . runProgram($testPrograms[6], $input) . "\n";
     }
-}
-
-function initRegisters(): array
-{
-    $input = file_get_contents(INPUT_FILE);
-    $lines = explode(',', $input);
-    return array_map(function ($x): int {
-        return (int) $x;
-    }, $lines);
-}
-
-function runProgram(array $registers, int $input): int
-{
-    $stepToExecute = 0;
-    $output = 0;
-
-    while ($registers[$stepToExecute] != OP_EXIT) {
-        $op = (string) $registers[$stepToExecute];
-        $opCode = (int) substr($op, -2);
-        // print "Execute $opCode from position $stepToExecute\n";
-        $params = getParams($opCode, $op, $stepToExecute, $registers);
-
-
-        $incrementStepCounter = true;
-        switch ($opCode) {
-            case OP_ADD:
-                $registers[$params[2]] = $params[0] + $params[1];
-                break;
-
-            case OP_MULTIPLY:
-                $registers[$params[2]] = $params[0] * $params[1];
-                break;
-
-            case OP_INPUT:
-                $registers[$params[0]] = $input;
-                break;
-
-            case OP_OUTPUT:
-                $output = $registers[$params[0]];
-                break;
-
-            case OP_JUMP_IF_TRUE:
-                if ($params[0] !== 0) {
-                    $stepToExecute = $params[1];
-                    $incrementStepCounter = false;
-                }
-                break;
-
-            case OP_JUMP_IF_FALSE:
-                if ($params[0] === 0) {
-                    $stepToExecute = $params[1];
-                    $incrementStepCounter = false;
-                }
-                break;
-
-            case OP_LESS_THAN:
-                $registers[$params[2]] = ($params[0] < $params[1]) ? 1 : 0;
-                break;
-
-            case OP_EQUALS:
-                $registers[$params[2]] = ($params[0] === $params[1]) ? 1 : 0;
-                break;
-
-            default:
-                die('Unexpected op code at position ' . $stepToExecute . ': ' . $registers[$stepToExecute] . "\n");
-        }
-
-        if ($incrementStepCounter) {
-            $stepToExecute = $stepToExecute + count($params) + 1;
-        }
-    }
-
-    return $output;
-}
-
-function getParams(int $opCode, string $op, int $stepToExecute, array $registers): array
-{
-    $parameterCount = getParameterCount($opCode);
-    $parameterModes = getParameterModes($op, $parameterCount);
-    $params = [];
-    for ($i = 0; $i < $parameterCount; $i++) {
-        $registerIndex = $stepToExecute + $i + 1;
-        // The last parameter is always a position
-        if ($i === $parameterCount - 1) {
-            $params[$i] = $registers[$registerIndex];
-        } elseif ($parameterModes[$i] === BY_VAL) {
-            $params[$i] = $registers[$registerIndex];
-        } else {
-            $paramPos = $registers[$registerIndex];
-            $params[$i] = $registers[$paramPos];
-        }
-    }
-
-    return $params;
-}
-
-function getParameterCount(int $opCode): int
-{
-    switch ($opCode) {
-        case OP_ADD:
-        case OP_MULTIPLY:
-        case OP_LESS_THAN:
-        case OP_EQUALS:
-            return 3;
-
-        case OP_JUMP_IF_TRUE:
-        case OP_JUMP_IF_FALSE:
-            return 2;
-
-        case OP_INPUT:
-        case OP_OUTPUT:
-            return 1;
-
-        default:
-            throw new Exception('Unexpected op code ' . $opCode);
-    }
-}
-
-function getParameterModes(string $op, int $parameterCount): array
-{
-    $modes = [];
-    // Remove the op code, to just leave the parameter modes
-    $op = substr($op, 0, strlen($op) - 2);
-    $len = strlen($op);
-    for ($i = 0; $i < $parameterCount; $i++) {
-        $pos = $len - $i - 1;
-        if ($pos >= 0) {
-            $modeChar = substr($op, $pos, 1);
-            $mode = $modeChar === "0" ? BY_REF : BY_VAL;
-        } else {
-            $mode = BY_REF;
-        }
-
-        $modes[$i] = $mode;
-    }
-
-    return $modes;
 }
